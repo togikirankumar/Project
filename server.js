1dc79e5b58457f58132e843c6a7336e010eaae3f
togikirankumar <coolkiran373@gmai.com>
2019-07-28 10:45:58 +0530

    gallery changes

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 //git init
 //git add .
-//git commit -m "First Commit"
+//git commit -m "First Commit"    
+//create heroku
+//git push heroku master
+//heroku logs --to check any errors in heroku 
+//
+
+/*git init
+git add filename.txt
+git commit -m "first commmit
+git status  --to check modificaions we have done
+git diff filname.txt  --to check the diff in the after file updation from previous one
+git checkout fileame.txt    ==to rollback the changes we have commited into the local repository
+git log --to check status"
+
+to add into git repos ------git remote add origin <paste ur of remote repos>
+then -----------------------git push -u origin master */
 
 
 const express=require("express");
